bde6ebf9bfe5870d2a5e2d1622fd6ecf9bc45bae
KARUN-499 <NEUTRONX1PIECE@gmail.com>
2026-06-04 18:14:57 +0530

    fix: move build command to shell script for Vercel

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter-sdk
+export PATH="$PATH:$(pwd)/flutter-sdk/bin"
+flutter config --enable-web
+flutter pub get
+flutter build web --release \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY \
+  --dart-define=RAZORPAY_KEY_ID=$RAZORPAY_KEY_ID
