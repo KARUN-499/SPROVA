37c81d37a482257636b1c9aa4b8f0d48d364d233
KARUN-499 <NEUTRONX1PIECE@gmail.com>
2026-06-04 21:54:38 +0530

    fix: use pinned repo flutter_sdk in build.sh to stabilize Vercel web builds

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter-sdk
-export PATH="$PATH:$(pwd)/flutter-sdk/bin"
+if [ -d "./flutter_sdk" ]; then
+  FLUTTER_DIR="./flutter_sdk"
+else
+  FLUTTER_DIR="./flutter-sdk"
+  if [ ! -d "$FLUTTER_DIR" ]; then
+    git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$FLUTTER_DIR"
+  fi
+fi
+
+export PATH="$PATH:$(pwd)/$FLUTTER_DIR/bin"
+flutter --version
 flutter config --enable-web
 flutter pub get
 flutter build web --release \
-  --dart-define=SUPABASE_URL=$SUPABASE_URL \
-  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY \
-  --dart-define=RAZORPAY_KEY_ID=$RAZORPAY_KEY_ID 2>&1
-echo "Exit code: $?"
+  --dart-define=SUPABASE_URL=${SUPABASE_URL:-} \
+  --dart-define=SUPABASE_ANON_KEY=${SUPABASE_ANON_KEY:-} \
+  --dart-define=RAZORPAY_KEY_ID=${RAZORPAY_KEY_ID:-} 2>&1
+ echo "Exit code: $?"
